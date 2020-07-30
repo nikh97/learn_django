9143de14970adc52b364007220e661bd753f6290
Nikhil Gosike <nikhilgosike@Nikhils-MBP-3.home>
2020-07-29 22:20:57 -0400

    rest now

--- djrestapi/djrestapi/urls.py
+++ djrestapi/djrestapi/urls.py
@@ -1,4 +1,4 @@
-"""djangorestapi URL Configuration
+"""djrestapi URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
