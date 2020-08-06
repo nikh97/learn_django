2f900efd975c01daa80372bff6a6454e5fbff671
Nikhil Gosike <nikhilgosike@Nikhils-MBP-3.home>
2020-08-05 23:44:22 -0400

    rest-part3

--- djrestapi/djrestapi/settings.py
+++ djrestapi/djrestapi/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'tutorial.apps.TutorialConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
